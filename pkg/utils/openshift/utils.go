5ae8015863cd27265455c733d08864fb005cb0c0
Jeremy Ary <jeremy.ary@gmail.com>
2019-06-11 08:32:19 -0500

    giving better example return for GetPlatformInfo yielding OCP 4.1, K8C 1.13+ to be more up to date

--- pkg/utils/openshift/utils.go
+++ pkg/utils/openshift/utils.go
@@ -17,7 +17,7 @@ func IsOpenShift(cfg *rest.Config) (bool, error) {
 GetPlatformInfo examines the Kubernetes-based environment and determines the running platform, version, & OS.
 Accepts <nil> or instantiated 'cfg' rest config parameter.
 
-Result: PlatformInfo{ Name: Kubernetes, OCPVersion: , K8SVersion: 1.14, OS: linux/amd64 }
+Result: PlatformInfo{ Name: OpenShift, OCPVersion: 4.1.0, K8SVersion: 1.13+, OS: linux/amd64 }
 */
 func GetPlatformInfo(cfg *rest.Config) (platform.PlatformInfo, error) {
 	return platform.K8SBasedPlatformVersioner{}.GetPlatformInfo(nil, cfg)
